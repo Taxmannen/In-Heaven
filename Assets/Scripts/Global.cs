a78ac33df448087d3daf1c793c0b7aade8fc8391
Efgeen <efgeen1998@gmail.com>
2019-01-17 14:02:28 +0100

    Script base scructure, fmod gitignore

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+
+    public enum PlayerState
+    {
+        Default,
+        Invincible,
+    }
+
+}
---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class InterfaceController : MonoBehaviour
+{
+
+    public static InterfaceController instance;
+
+    [SerializeField] private Text playerStateText;
+
+    private void Awake()
+    {
+
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            enabled = true;
+        }
+
+    }
+
+    public void UpdatePlayerState(Global.PlayerState playerState)
+    {
+        playerStateText.text = "PlayerState: " + playerState;
+    }
+
+}
