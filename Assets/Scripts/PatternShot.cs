4b663cdb6a14b52cdaa6414dfe69adcf2f5dd1c0
Jonatan Karlsson <JK98@Live.se>
2019-01-31 14:02:13 +0100

    Patternshot under development

---
+++ Assets/Scripts/PatternShot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternShot : MonoBehaviour
+{
+
+
+  
+
+    private IEnumerator PatternShotCorutine(Vector3 PatternShotSpawnPosition, Vector3 PatternShotTargetPoint)
+    {
+       //string[] textFile = Assets.Resources.TextAssets.PatternShot1.txt;
+            var textFile = Resources.Load<TextAsset>("TextAssets/PatternShots1");
+
+
+
+        //string[] patternString = "Assets/TextAssets.pattern1";
+
+        int PatternShot =
+
+
+        yield break;
+    }
+
+}
