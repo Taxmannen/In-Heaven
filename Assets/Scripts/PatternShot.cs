efd893a935bfa47665d75f5ad94b07fbee478032
Jonatan Karlsson <JK98@Live.se>
2019-01-31 14:03:18 +0100

    TokenCommit

--- Assets/Scripts/PatternShot.cs
+++ Assets/Scripts/PatternShot.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class PatternShot : MonoBehaviour
 {
-
+    public TextAsset textFile;
+    string[] patternString;
 
   
+
+    public void PatternGenerateArray()
+    {
+        patternString = (textFile.text.Split('\n'));
+    }
 
     private IEnumerator PatternShotCorutine(Vector3 PatternShotSpawnPosition, Vector3 PatternShotTargetPoint)
     {
        //string[] textFile = Assets.Resources.TextAssets.PatternShot1.txt;
             var textFile = Resources.Load<TextAsset>("TextAssets/PatternShots1");
 
+        float[] patternLength = new float[patternString.Length];
+
+        float patternSpread = float.Parse(patternString[0]);
+
+        float patternShotTargetX = float.Parse(patternString[1]);
+        float patternShotTargetY = float.Parse(patternString[2]);
+
+        Vector3 PatternStartingPoint = new Vector3();
+        Vector3 PatternShotTarget = new Vector3(patternShotTargetX, patternShotTargetY, 0);
 
 
-        //string[] patternString = "Assets/TextAssets.pattern1";
+        for (int i = 3; i < patternLength.Length; i++)
+        {
 
-        int PatternShot =
+            Debug.Log(patternString[i]);
 
+            if (float.Parse(patternString[i]) == 0)
+            {
+
+            }
+            if(float.Parse(patternString[i]) == 1)
+            {
+
+            }
+
+
+        }
 
         yield break;
     }
