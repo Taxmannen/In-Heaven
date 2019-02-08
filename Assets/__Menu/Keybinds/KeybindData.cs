aaa4f20c0f89d3f00d8d5156f018f169c056cec6
Efgeen <efgeen1998@gmail.com>
2019-02-08 14:46:13 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/__Menu/Keybinds/KeybindData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class KeybindData : MonoBehaviour
+{
+
+    public static KeybindData instance;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public enum Keyfunc
+    {
+        Left,
+        Right,
+        Jump,
+        Tempname,
+        Dash,
+        ShootParry,
+        Shoot,
+        Parry,
+    }
+
+    private Text text;
+
+    public void OCText(Text text)
+    {
+        this.text = text;
+    }
+
+    private Keyfunc keyFunc;
+
+    public void SelectedKey(Keyfunc func)
+    {
+        keyFunc = func;
+    }
+
+    public void SavePrimary()
+    {
+
+    }
+
+    public void SaveSecondary()
+    {
+
+    }
+
+}
