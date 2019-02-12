d39e139effdae96934933e0b07628f51ed51d6da
Anton <a.e.lindkvist@gmail.com>
2019-02-12 13:32:54 +0100

    Added new prefab for BossLaserRay to make vfx work

--- Assets/Scripts/BossLaser.cs
+++ Assets/Scripts/BossLaser.cs
@@ -6,36 +6,18 @@ public class BossLaser : MonoBehaviour
 {
     float damage;
 
-    internal Material material;
-
     [SerializeField]
     internal ParticleSystem vfx;
 
-    [SerializeField]
-    internal float horizontalScrollSpeedMultiplier = 0;
-
-    [SerializeField]
-    internal float verticalScrollSpeedMultiplier = 1;
-
-    internal Vector2 textureScrollingOffset = new Vector2(0, 1);
+    private void Start()
+    {
+        vfx = GetComponentInChildren<ParticleSystem>();
+        vfx.Play();
+    }
 
     public void SetDamage(float damage)
     {
         this.damage = damage;
-    }
-
-    private void Start()
-    {
-        material = GetComponent<Renderer>().material;
-        vfx = GetComponentInChildren<ParticleSystem>();
-        vfx.Play(); 
-    }
-
-    private void Update()
-    {
-        textureScrollingOffset.x += Time.deltaTime * horizontalScrollSpeedMultiplier;
-        textureScrollingOffset.y += Time.deltaTime * verticalScrollSpeedMultiplier;
-        material.SetTextureOffset("_MainTex", textureScrollingOffset);
     }
 
     private void OnTriggerEnter(Collider other)
---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+
+    [SerializeField]
+    internal Material material;
+
+    [SerializeField]
+    internal ParticleSystem vfx;
+
+    [SerializeField]
+    internal float horizontalScrollSpeedMultiplier = 0;
+
+    [SerializeField]
+    internal float verticalScrollSpeedMultiplier = 1;
+
+    internal Vector2 textureScrollingOffset = new Vector2(0, 1);
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        vfx = GetComponentInChildren<ParticleSystem>();
+        vfx.Play();
+    }
+
+    private void Update()
+    {
+        textureScrollingOffset.x += Time.deltaTime * horizontalScrollSpeedMultiplier;
+        textureScrollingOffset.y += Time.deltaTime * verticalScrollSpeedMultiplier;
+        material.SetTextureOffset("_MainTex", textureScrollingOffset);
+    }
+}
