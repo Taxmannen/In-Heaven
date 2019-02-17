190e94d278291cf6cb9a74e218a939cccc645e2e
Jonatan <JK98@Live.se>
2019-02-17 23:30:42 +0100

    Screen changer

---
+++ Assets/BossFaceController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFaceController : MonoBehaviour
+{
+    public Material[] screenMats;
+    Renderer newFace;
+
+    private float timer=0;
+
+    void Start()
+    {
+      //screenMaterial1 = GetComponent<Renderer>().material;
+
+
+        newFace = GetComponent<Renderer>();
+        newFace.enabled = true;
+        newFace.sharedMaterial = screenMats[0];
+    }
+
+   
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if (timer>=4 && timer<=8)
+        {
+            newFace.sharedMaterial = screenMats[1];
+        }
+        else if (timer >=8 && timer <=12)
+        {
+            newFace.sharedMaterial = screenMats[2];
+        }
+        else if (timer >= 12 && timer <= 16)
+        {
+            newFace.sharedMaterial = screenMats[3];
+        }
+        else if (timer >=16)
+        {
+            newFace.sharedMaterial = screenMats[4];
+        }
+
+    }
+}
---
+++ Assets/StaticScreen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticScreen : MonoBehaviour
+{
+
+    public int staticDuration;
+
+    public Material[] staticMats;
+    Renderer rendStatic;
+
+    void Start()
+    {
+        rendStatic = GetComponent<Renderer>();
+        rendStatic.enabled = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void staticmethod()
+    {
+        StartCoroutine(staticScreen());
+    }
+
+    IEnumerator staticScreen()
+    {
+        for (int i = 0; i < staticDuration; i++)
+        {
+            rendStatic.sharedMaterial = staticMats[0];
+            yield return new WaitForEndOfFrame();
+            rendStatic.sharedMaterial = staticMats[1];
+            yield return new WaitForEndOfFrame();
+            rendStatic.sharedMaterial = staticMats[2];
+            yield return new WaitForEndOfFrame();
+            rendStatic.sharedMaterial = staticMats[3];
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+}
