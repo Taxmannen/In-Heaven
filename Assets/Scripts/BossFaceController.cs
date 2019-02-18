29b020f8b6b374e5ce6533d3d66e5b6770fb022e
Jonatan Karlsson <JK98@Live.se>
2019-02-18 16:38:40 +0100

    Nu fan funkar den satans skiten
    Myrornas krig startar när bossen tar skada, fiffla med staticDuration på Head för att ändra längden

--- Assets/Scripts/BossFaceController.cs
+++ Assets/Scripts/BossFaceController.cs
@@ -14,14 +14,7 @@ public class BossFaceController : MonoBehaviour
     private float timer=0;
     int screenController;
 
-    private bool s1 = true;
-    private bool s2 = true;
-    private bool s3 = true;
-    private bool s4 = true;
-    private bool s5 = true;
-    private bool s6 = true;
-    private bool s7 = true;
-    private bool s8 = true;
+
 
     void Start()
     {
@@ -36,7 +29,7 @@ public class BossFaceController : MonoBehaviour
         rendStatic.enabled = true;
     }
 
-    void updateScreen(int screenController)
+    public void updateScreen(int screenController)
     {
         StartCoroutine(staticScreen(screenController));
     }
@@ -46,7 +39,7 @@ public class BossFaceController : MonoBehaviour
     IEnumerator staticScreen(int screenController)
     {
         for (int i = 0; i < staticDuration; i++)
-       // while(true)
+      
         {
             rendStatic.sharedMaterial = staticMats[0];
             yield return new WaitForSeconds(0.1f);
@@ -63,73 +56,9 @@ public class BossFaceController : MonoBehaviour
             rendStatic.sharedMaterial = staticMats[3];
             yield return new WaitForSeconds(0.1f);
 
-            //staticMats[0]= null;
-            //staticMats[1]= null;
-            //staticMats[2]= null;
-            //staticMats[3]= null;
-         
-            // yield return null;
         }
-
-        
-       
-        
-        
 
         newFace.sharedMaterial = screenMats[screenController];
         yield break;
-    }
-
-
-
-
-    void Update()
-    {
-        timer += Time.deltaTime;
-        
-        if (timer>=4 && timer<=8&&s1==true)
-        {
-            //newFace.sharedMaterial = screenMats[1];
-            updateScreen(1);
-            s1 = false;
-        }
-        else if (timer >=8 && timer <=12 && s2==true)
-        {
-            s2 = false;
-            //newFace.sharedMaterial = screenMats[2];
-            updateScreen(2);
-
-        }
-        else if (timer >= 12 && timer <= 16 && s2 == true)
-        {
-            s3 = false;
-            //newFace.sharedMaterial = screenMats[3];
-            updateScreen(3);
-        }
-        else if (timer >= 16 && timer <= 20 && s2 == true)
-        {
-            s4 = false;
-            // newFace.sharedMaterial = screenMats[4];
-            updateScreen(4);
-        }
-        else if (timer >= 20 && timer <= 24 && s2 == true)
-        {
-            s5 = false;
-            //newFace.sharedMaterial = screenMats[5];
-            updateScreen(5);
-        }
-        else if (timer >= 24 && timer <= 28 && s2 == true)
-        {
-            s6 = false;
-            //newFace.sharedMaterial = screenMats[6];
-            updateScreen(6);
-        }
-        else if (timer >= 28 && s2 == true)
-        {
-            s7 = false;
-            //newFace.sharedMaterial = screenMats[7];
-            updateScreen(7);
-        }
-
     }
 }
