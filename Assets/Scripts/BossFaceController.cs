f5e24246cca0324e98c6a6fba7b24d4f09a2bd5f
Jonatan Karlsson <JK98@Live.se>
2019-02-18 15:33:15 +0100

    La till myrornas krig effekt

--- Assets/Scripts/BossFaceController.cs
+++ Assets/Scripts/BossFaceController.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class BossFaceController : MonoBehaviour
 {
     public Material[] screenMats;
-    Renderer newFace;
+    public Material[] staticMats;
 
+    Renderer newFace;
+    Renderer rendStatic;
+
+    public int staticDuration;
     private float timer=0;
+    int screenController;
+
+    private bool s1 = true;
+    private bool s2 = true;
+    private bool s3 = true;
+    private bool s4 = true;
+    private bool s5 = true;
+    private bool s6 = true;
+    private bool s7 = true;
+    private bool s8 = true;
 
     void Start()
     {
@@ -17,40 +31,104 @@ public class BossFaceController : MonoBehaviour
         newFace = GetComponent<Renderer>();
         newFace.enabled = true;
         newFace.sharedMaterial = screenMats[0];
+
+        rendStatic = GetComponent<Renderer>();
+        rendStatic.enabled = true;
     }
 
-   
+    void updateScreen(int screenController)
+    {
+        StartCoroutine(staticScreen(screenController));
+    }
+
+
+
+    IEnumerator staticScreen(int screenController)
+    {
+        for (int i = 0; i < staticDuration; i++)
+       // while(true)
+        {
+            rendStatic.sharedMaterial = staticMats[0];
+            yield return new WaitForSeconds(0.1f);
+           
+
+            rendStatic.sharedMaterial = staticMats[1];
+            yield return new WaitForSeconds(0.1f);
+
+            
+            rendStatic.sharedMaterial = staticMats[2];
+            yield return new WaitForSeconds(0.1f);
+           
+
+            rendStatic.sharedMaterial = staticMats[3];
+            yield return new WaitForSeconds(0.1f);
+
+            //staticMats[0]= null;
+            //staticMats[1]= null;
+            //staticMats[2]= null;
+            //staticMats[3]= null;
+         
+            // yield return null;
+        }
+
+        
+       
+        
+        
+
+        newFace.sharedMaterial = screenMats[screenController];
+        yield break;
+    }
+
+
+
+
     void Update()
     {
         timer += Time.deltaTime;
         
-        if (timer>=4 && timer<=8)
+        if (timer>=4 && timer<=8&&s1==true)
         {
-            newFace.sharedMaterial = screenMats[1];
+            //newFace.sharedMaterial = screenMats[1];
+            updateScreen(1);
+            s1 = false;
         }
-        else if (timer >=8 && timer <=12)
+        else if (timer >=8 && timer <=12 && s2==true)
         {
-            newFace.sharedMaterial = screenMats[2];
+            s2 = false;
+            //newFace.sharedMaterial = screenMats[2];
+            updateScreen(2);
+
         }
-        else if (timer >= 12 && timer <= 16)
+        else if (timer >= 12 && timer <= 16 && s2 == true)
         {
-            newFace.sharedMaterial = screenMats[3];
+            s3 = false;
+            //newFace.sharedMaterial = screenMats[3];
+            updateScreen(3);
         }
-        else if (timer >= 16 && timer <= 20)
+        else if (timer >= 16 && timer <= 20 && s2 == true)
         {
-            newFace.sharedMaterial = screenMats[4];
+            s4 = false;
+            // newFace.sharedMaterial = screenMats[4];
+            updateScreen(4);
         }
-        else if (timer >= 20 && timer <= 24)
+        else if (timer >= 20 && timer <= 24 && s2 == true)
         {
-            newFace.sharedMaterial = screenMats[5];
+            s5 = false;
+            //newFace.sharedMaterial = screenMats[5];
+            updateScreen(5);
         }
-        else if (timer >= 24 && timer <= 28)
+        else if (timer >= 24 && timer <= 28 && s2 == true)
         {
-            newFace.sharedMaterial = screenMats[6];
+            s6 = false;
+            //newFace.sharedMaterial = screenMats[6];
+            updateScreen(6);
         }
-        else if (timer >= 28)
+        else if (timer >= 28 && s2 == true)
         {
-            newFace.sharedMaterial = screenMats[7];
+            s7 = false;
+            //newFace.sharedMaterial = screenMats[7];
+            updateScreen(7);
         }
 
     }
