87236ec266321960f601c352696f23ef595f62b7
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2019-02-21 19:42:46 +0100

    When boss REAL dies it will go to Main Menu

--- Assets/Boss Script(s)/_Don't Touch/Boss.cs
+++ Assets/Boss Script(s)/_Don't Touch/Boss.cs
@@ -439,6 +439,7 @@ public class Boss : Character
             activePhase++;
             base.Die();
             InterfaceController.instance.HideBossHPBar();
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
         }
 
     }
